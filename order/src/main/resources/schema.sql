a28d1eb90588f8705d4c22bcacae422ca4f3d9a4
Hwang Seo Jeong <129826956+hsj1024@users.noreply.github.com>
2024-12-04 17:14:43 +0900

    Update schema.sql

--- order/src/main/resources/schema.sql
+++ order/src/main/resources/schema.sql
@@ -1,5 +1,4 @@
 -- CREATE DATABASE IF NOT EXISTS orderdb;
 USE orderdb;
 ALTER DATABASE orderdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE product CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE order_item CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
