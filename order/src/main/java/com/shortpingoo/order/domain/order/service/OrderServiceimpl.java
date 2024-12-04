2f6c0467a907ad79679b29c43bc4e1ee682502cb
Hwang Seo Jeong <129826956+hsj1024@users.noreply.github.com>
2024-12-05 02:08:28 +0900

    fix OrderServiceimpl.java - 배포 테스트

--- order/src/main/java/com/shortpingoo/order/domain/order/service/OrderServiceimpl.java
+++ order/src/main/java/com/shortpingoo/order/domain/order/service/OrderServiceimpl.java
@@ -123,7 +123,7 @@ public class OrderServiceimpl implements OrderService {
         List<Map<String, Object>> products;
         try {
             ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-                    "/api/brand/product/owner", // Gateway로 라우팅된 엔드포인트
+                    "https://shortpingoo.shop/api/brand/product/owner", // Gateway로 라우팅된 엔드포인트
                     HttpMethod.GET,
                     entity,
                     new ParameterizedTypeReference<List<Map<String, Object>>>() {}
